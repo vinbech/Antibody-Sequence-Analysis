bb58059f9ffbcf7cb89007fb173ff88d256ca9d2
Draaguah <ashaugaard@hotmail.com>
2024-12-05 12:36:02 +0100

    Add files via upload

--- humanization.py
+++ humanization.py
@@ -1,41 +1,28 @@
 from Bio import SeqIO
 from Bio import Align
 
-human = [s for s in SeqIO.parse('project_sequences/human.fa', 'fasta')]
-mouse = [s for s in SeqIO.parse('project_sequences/mouse.fa', 'fasta')]
-
+genomes = ['human', 'mouse']
+db = [[s for s in SeqIO.parse(f'project_sequences/{genome}.fa', 'fasta')] for genome in genomes]
 input = "QQGNVFSCSVMHEALHNHYTQKSLSLSPGK"
-#7595
 
 bestScore = -1
 bestID = -1
-genome = ""
+species = ""
 bestAlign = None
 
 aligner = Align.PairwiseAligner()
 aligner.mode = 'global'
 
-for sequence in human:
-    alignments = aligner.align(sequence.seq, input)
-    alignment = alignments[0]
-    score = alignment.score
-    if score > bestScore:
-        bestScore = score
-        bestID = sequence.id
-        genome = 'Human'
-        bestAlign = alignment
-
-for sequence in mouse:
-    alignments = aligner.align(sequence.seq, input)
-    alignment = alignments[0]
-    score = alignment.score
-    if score > bestScore:
-        bestScore = score
-        bestID = sequence.id
-        genome = 'Mouse'
-        bestAlign = alignment
+for i, genome in enumerate(db):
+    for sequence in genome:
+        alignment = aligner.align(sequence.seq, input)[0]
+        if alignment.score > bestScore:
+            bestScore = alignment.score
+            bestID = sequence.id
+            species = genomes[i]
+            bestAlign = alignment
 
 print(f'ID: {bestID}')
 print(f'Score: {bestScore}')
-print(f'Genome: {genome}')
+print(f'Genome: {species}')
 print(bestAlign)
